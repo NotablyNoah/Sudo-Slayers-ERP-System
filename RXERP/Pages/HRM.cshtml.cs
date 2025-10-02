cff6b1c3eb199d19603353512fd43e41362adad0
jacobpowers01 <jacob_powers@icloud.com>
2025-10-02 14:09:48 -0500

    massive code drop
    im going to commit

---
+++ RXERP/Pages/HRM.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RXERP.Pages;
+
+public class HRMModel : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
