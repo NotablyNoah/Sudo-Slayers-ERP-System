8f3050404ec26e76cae9804ef71fe03ce53a625b
jacobpowers01 <jacob_powers@icloud.com>
2025-10-21 14:20:22 -0500

    Table Update and Privacy Page Change
    Updated table to include more users
    Privacy page changed to incomplete calculator for some reason

--- RXERP/Pages/Privacy.cshtml.cs
+++ RXERP/Pages/Privacy.cshtml.cs
@@ -5,13 +5,41 @@ namespace RXERP.Pages;
 
 public class PrivacyModel : PageModel
 {
-    private readonly ILogger<PrivacyModel> _logger;
+    [BindProperty] public double num1 { get; set; }
+    [BindProperty] public double num2 { get; set; }
+    [BindProperty] public string? operation { get; set; }
 
-    public PrivacyModel(ILogger<PrivacyModel> logger)
+    public double? result { get; set; }
+
+    public IActionResult OnPost()
     {
-        _logger = logger;
+        switch (operation)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "x":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Cannot divide by zero.");
+                }
+                break;
+            default:
+                ModelState.AddModelError(string.Empty, "Invalid operation.");
+                break;
+        }
+        return Page();
     }
-
     public void OnGet()
     {
     }
